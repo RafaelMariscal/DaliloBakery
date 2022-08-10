6109df25be3479670a4613e027a937f237242029
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-08-10 10:09:55 -0300

    Feat/Page not found fully integrated.

--- src/Pages/NotFound/index.tsx
+++ src/Pages/NotFound/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom'
+import NotFoundImg from '../../Assets/notFoundImg.svg'
+import LogoDalilo from '../../Assets/LogoDalilo.svg'
+import styles from './styles.module.scss'
+import { AiFillShopping } from 'react-icons/ai'
 function NotFound() {
   return (
-    <h1>Not Found</h1>
+    <div className={styles.container}>
+      <header className={styles.checkoutHeader}>
+        <div>
+          <div>
+            <Link to={'/'}>
+              <img src={LogoDalilo} alt="" />
+
+              <div>
+                <h1>Forninho Dalilo</h1>
+                <p>A melhor Boutique de Pães Artesanais do Brasil!</p>
+              </div>
+            </Link>
+          </div>
+
+          <Link to={'/'} className={styles.linkToHome}>
+            Return to Shop
+            <AiFillShopping size={24} />
+          </Link>
+
+        </div>
+
+      </header>
+
+      <Link to={'/'}>
+        <img src={NotFoundImg} alt="" />
+
+        <Link to={'/'} className={styles.linkToHome}>
+          Return to Shop
+        </Link>
+      </Link>
+    </div>
   )
 }
 
